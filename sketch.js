956b39a7a606614c5ebfcdc02e85c158f61b5fb5
abhay024 <121883043+abhay024@users.noreply.github.com>
2024-05-20 11:34:02 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,16 +8,16 @@ var fuels,goldCoins,obstacles;
 var fuel_img, goldCoin_img, obstacle1_img, obstacle2_img, life_img, blast_img;
 
 function preload() {
-  backgroundImage = loadImage("../assets/background.png");
-  car1_img = loadImage("../assets/car1.png");
-  car2_img = loadImage("../assets/car2.png");
-  track = loadImage("../assets/track.jpg");
-  fuel_img = loadImage('../assets/fuel.png');
-  goldCoin_img = loadImage('../assets/goldCoin.png');
-  obstacle1_img = loadImage('../assets/obstacle1.png');
-  obstacle2_img = loadImage('../assets/obstacle2.png');
-  life_img = loadImage('../assets/life.png');
-  blast_img = loadImage('../assets/blast.png');
+  backgroundImage = loadImage("./assets/background.png");
+  car1_img = loadImage("./assets/car1.png");
+  car2_img = loadImage("./assets/car2.png");
+  track = loadImage("./assets/track.jpg");
+  fuel_img = loadImage('./assets/fuel.png');
+  goldCoin_img = loadImage('./assets/goldCoin.png');
+  obstacle1_img = loadImage('./assets/obstacle1.png');
+  obstacle2_img = loadImage('./assets/obstacle2.png');
+  life_img = loadImage('./assets/life.png');
+  blast_img = loadImage('./assets/blast.png');
 }
 
 function setup() {
